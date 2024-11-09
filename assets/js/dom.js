32e6ce3caab0857410e2a538a60d09da45254c3b
SimpleCodeCraft <weniaminwelichko@gmail.com>
2024-11-09 13:27:16 +0200

    Initial commit

---
+++ assets/js/dom.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const board = document.getElementsByClassName('board')[0];
+
+console.log(board);
+
+let oldSelectedElement;
+
+board.onclick = function(event){
+    let target = event.target; // где был клик?
+
+    // if (target.tagName != 'TD') return; // не на TD? тогда не интересует
+
+    if(oldSelectedElement) oldSelectedElement.style.backgroundColor = '#f5f5f5';
+
+    console.log(oldSelectedElement);
+
+    target.style.backgroundColor = 'red';
+    oldSelectedElement = target;
+}
+
+
+function highlight(td) {
+    if (td.classList.high) { // убрать существующую подсветку, если есть
+      selectedTd.classList.remove('highlight');
+    }
+    selectedTd = td;
+    selectedTd.classList.add('highlight'); // подсветить новый td
+  }
+  
+
+ 
